977da28a7af4b6192408c3ccec9e40bdc944609f
auroraborealis2002 <melancholymethane@gmail.com>
2021-01-29 08:50:19 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,39 @@
 # Caching the Inverse of a Matrix
 
-# Cache the matrix, and calculate its inverse matrix
-makeCacheMatrix <- function(x = matrix()) {
-    cachedMatrix <<- x
-    invertedMatrix <<- solve(x)
+# Generate a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(cachedMatrix = matrix()) {
+    invertedMatrix <<- NULL
+    set <- function(y) {
+        cachedMatrix <<- y
+        invertedMatrix <<- NULL
+    }
+    get <- function() cachedMatrix
+    setInvert <- function(input) invertedMatrix <<- input
+    getInvert <- function() invertedMatrix
+    list(set = set, get = get, setInvert = setInvert, getInvert = getInvert)
 }
 
 # Print the inverted matrix of x:
 # **If x has been cached, print the cached matrix directly.
-# **Otherwise, update the cache, calculate and print the new inverted matrix.
+# **Otherwise, calculate, cache and print the new inverted matrix.
 cacheSolve <- function(x, ...) {
-    if (exists("cachedMatrix") && identical(x, cachedMatrix)) {
+    inverted <- x$getInvert()
+    if (!is.null(inverted)) {
         message("CACHED:")
         return(invertedMatrix)
     }
-    makeCacheMatrix(x)
+    data <- x$get()
+    calcInvert <- solve(data)
+    x$setInvert(calcInvert)
     message("NEW:")
-    invertedMatrix
+    calcInvert
 }
+
+# Example:
+# > mymat <- makeCacheMatrix(matrix(c(1,7,9,4,6,2,8,11,3), 3, 3))
+# > cacheSolve(mymat)
+# > cacheSolve(mymat)
+# When a new matrix has been set
+# > mymat$set(matrix(c(19,7,9,4), 2, 2))
+# > cacheSolve(mymat)
+# > cacheSolve(mymat)
